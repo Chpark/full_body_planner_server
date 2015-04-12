19981c1e76c3f0feac303c88d9fe92e427648dc1
Chonhyon Park <chpark@cs.unc.edu>
2015-04-12 04:19:21 -0400

    use stdout buffer

--- full_body_planner_server/src/full_body_planner_server.cpp
+++ full_body_planner_server/src/full_body_planner_server.cpp
@@ -537,7 +537,7 @@ void FullBodyPlannerServer::deallocateResponseMemory()
 int main(int argc, char **argv)
 {
     // for debug
-    setbuf(stdout, NULL);
+    //setbuf(stdout, NULL);
 
     ros::init(argc, argv, "full_body_planner");
     ros::AsyncSpinner spinner(1);
